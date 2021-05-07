7130a5c09bfb059d99e0572474d84e1a0909ee0b
gizem <gizemsacal@gmail.com>
2021-05-07 20:29:14 +0300

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,27 @@
+package com.company;
+
+import java.util.TreeSet;
+
+class Main {
+    public static void main(String[] args) {
+        TreeSet<Integer> numbers = new TreeSet<>();
+        numbers.add(2);
+        numbers.add(5);
+        numbers.add(4);
+        numbers.add(6);
+        System.out.println("TreeSet: " + numbers);
+
+        // Using higher()
+        System.out.println("Using higher: " + numbers.higher(4));
+
+        // Using lower()
+        System.out.println("Using lower: " + numbers.lower(4));
+
+        // Using ceiling()
+        System.out.println("Using ceiling: " + numbers.ceiling(4));
+
+        // Using floor()
+        System.out.println("Using floor: " + numbers.floor(3));
+
+    }
+}
